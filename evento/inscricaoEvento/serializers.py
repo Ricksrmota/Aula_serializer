9f49e2ca242946b873a16e4bd713a3eabb35775f
RICARDO DE SOUSA RODRIGUES MOTA <108013210@labin.canoas>
2017-10-30 21:19:44 -0300

    Serializer POST

--- evento/inscricaoEvento/serializers.py
+++ evento/inscricaoEvento/serializers.py
@@ -9,23 +9,37 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = User
 		fields = ('url', 'username', 'email')
+	def create(self, validated_data):
+		user = User.objects.create(**validated_data)
+		return user
 
 class PessoaSerializer(serializers.HyperlinkedModelSerializer):
 	usuario = UserSerializer(many=False)
 	class Meta:
 		model = Pessoa
 		fields = ('usuario','nome', 'sexo', 'idade')
+	def create(self, dados):
+		pessoa_data = dados.pop('usuario') #remove o relacionamento de usuario com a pessoa
+		u = User.objects.create(**pessoa_data) #cria o objeto de usuario separadamente
+		p = Pessoa.objects.create(usuario=u, **dados) #a tag de usuario tem que ser a mesma do models, aqui cria a pessoa e passa ambos!
+		return p
 
 class EventoSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = Evento
 		fields = '__all__'
+	def create(self, dados):
+		event = Evento.objects.create(**dados)
+		return event
 
 class TicketSerializer(serializers.HyperlinkedModelSerializer):
 	evento = EventoSerializer(many=False)
 	class Meta:
 		model = Ticket
 		fields = '__all__'
+	def create(self, dados):
+		tick = Ticket.objects(**dados)
+		return tick
 
 class InscricaoSerializer(serializers.HyperlinkedModelSerializer):
 	participante = PessoaSerializer(many=False)
